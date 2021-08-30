53e301a3f181cedebfac916a6c153fd219d077b6
toxic-development <63737517+buffkermitisagod@users.noreply.github.com>
2021-08-31 00:29:39 +0100

    Add files via upload

---
+++ huwei_unlock_broot.py
@@ -0,0 +1,37 @@
+import itertools
+import os
+import subprocess
+
+n = 9
+while True:
+    pro = itertools.product("1234567890", repeat=n)
+    x = 0
+
+    os.system("adb reboot fastboot")
+    for i in pro:
+        pas = str(i)
+        pas = pas.replace("')","")
+        pas = pas.replace("('","")
+        pas = pas.replace("'","")
+        pas = pas.replace(" ","")
+        pas = pas.replace(",","")
+
+        print("trying pin: "+pas)
+
+        co = "fastboot oem unlock "+pas 
+
+        output = subprocess.run(co, shell=True, capture_output=True, text=True)
+
+        output = output.stdout
+        output = str(output)
+
+        if 'success' in output:
+            bak = open("unlock_code.txt", "w")
+            bak.write("Your saved bootloader code : "+pas)
+            bak.close()
+            print("Your bruteforce result has been saved in \"unlock_code.txt\"")
+
+        else:
+            print("Rebooting to prevent bootloader from rebooting...")
+            os.system('fastboot reboot bootloader')
+    n = n + 1
